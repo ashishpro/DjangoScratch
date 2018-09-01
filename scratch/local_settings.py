3b544d8dd82f4c8619c7c1bd36ea06accd4fea53
Ashish_Kumar <ashishsidhu19996@gmail.com>
2018-09-02 02:09:49 +0530

    First Commit

---
+++ scratch/local_settings.py
@@ -0,0 +1,11 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scratch',
+        'USER': 'devash',
+        'PASSWORD': 'devash',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
